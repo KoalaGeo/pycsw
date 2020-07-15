17f7e2535e50687deae59bbcfae4631223efc841
koalageo <eddlewis85@gmail.com>
2020-07-15 10:43:55 +0100

    run

---
+++ pycsw/bin/pycsw-admin.py
@@ -0,0 +1,7 @@
+#!/home/edd/Documents/GitKraken/pycsw/pycsw/bin/python
+# EASY-INSTALL-DEV-SCRIPT: 'pycsw==2.5.dev0','pycsw-admin.py'
+__requires__ = 'pycsw==2.5.dev0'
+__import__('pkg_resources').require('pycsw==2.5.dev0')
+__file__ = '/home/edd/Documents/GitKraken/pycsw/bin/pycsw-admin.py'
+with open(__file__) as f:
+    exec(compile(f.read(), __file__, 'exec'))
